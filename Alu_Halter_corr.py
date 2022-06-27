e2ab54bc0a71f3759b60a261e8635db8f56de0ff
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2022-06-27 10:43:44 +0200

    Add files via upload

--- Alu_Halter_corr.py
+++ Alu_Halter_corr.py
@@ -1,5 +1,5 @@
 """
-Created 24. June 2022 by Daniel Van Opdenbosch, Technical University of Munich
+Created 27. June 2022 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -8,10 +8,14 @@ import os
 import sys
 import glob
 import numpy
-import matplotlib.pyplot as plt
 import lmfit
+import matplotlib.pyplot as plt
+import xrayutilities as xu
 from scipy import interpolate
 from scipy import signal
+
+def fsquared(vects,atoms,energy):												#Atomare Streufaktoren
+	return numpy.real(numpy.average(numpy.array([i.f(2*numpy.pi*vects,en=energy) for i in atoms])**2,axis=0))
 
 if len(sys.argv)>1:
 	filepattern=sys.argv[1]
@@ -42,17 +46,20 @@ for i in files:
 			argscut.append(numpy.arange(i-int(1/2/step),i+int(1/2/step)))
 	twotheta_deg,yobs,yh=numpy.delete(twotheta_deg,argscut),numpy.delete(yobs,argscut),numpy.delete(yh,argscut)
 
-	yobsforfit,yh=signal.savgol_filter(yobs,101,1),signal.savgol_filter(yh,101,1)
+	yh,emission=signal.savgol_filter(yh,101,1),'CuKa1'
+	Lfsq=	1/(2*numpy.sin(numpy.radians(twotheta_deg/2))*numpy.sin(numpy.radians(twotheta_deg)))*\
+			fsquared(2*numpy.sin(numpy.radians(twotheta_deg/2))/xu.utilities_noconf.wavelength(emission),[xu.materials.atom.Atom('C',1)],xu.utilities_noconf.energy(emission))
 	params=lmfit.Parameters()
 	params.add('Cyh',1)
 	def minfunc(params):
 		prm=params.valuesdict()
-		return numpy.gradient(yobsforfit-prm['Cyh']*yh)*(1+numpy.sum((prm['Cyh']*yh-yobsforfit)[numpy.where(prm['Cyh']*yh>yobsforfit)]))
-	result=lmfit.minimize(minfunc,params,method='nelder')
+		return (Lfsq/Lfsq[-1]-(yobs-prm['Cyh']*yh)/(yobs-prm['Cyh']*yh)[-1])[numpy.where(twotheta_deg>45)]
+	result=lmfit.minimize(minfunc,params)
 	prm=result.params.valuesdict()
 	yobs-=prm['Cyh']*yh
 
 	plt.plot(twotheta_deg,yobs)
+	plt.plot(twotheta_deg,Lfsq/Lfsq[-1]*yobs[-1])
 	plt.plot(twotheta_deg,prm['Cyh']*yh)
 	plt.text(min(twotheta_deg),min(prm['Cyh']*yh),prm['Cyh'].round(2))
 
