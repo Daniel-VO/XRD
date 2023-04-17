e776953948527563213f4ad495f8977310bc01d5
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2023-04-17 09:19:12 +0200

    Add files via upload

---
+++ Halter_corr.py
@@ -0,0 +1,54 @@
+"""
+Created 01. February 2023 by Daniel Van Opdenbosch, Technical University of Munich
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
+"""
+
+import os
+import sys
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from scipy import signal
+
+if len(sys.argv)>1:
+	filepattern=sys.argv[1]
+else:
+	filepattern=''
+os.system('rm '+os.path.splitext(filepattern)[0]+'*_corr.png')
+os.system('rm '+os.path.splitext(filepattern)[0]+'*_bgcorr.xy')
+
+tt,yh0=np.genfromtxt('Alu_Halter_flach_Miniflex.xy',unpack=True)
+f=interpolate.interp1d(tt,yh0)
+
+for i in list(filter(lambda a:'Halter' not in a,glob.glob(os.path.splitext(filepattern)[0]+'*.xy'))):
+	filename=os.path.splitext(i)[0]
+	twotheta_deg,yobs=np.genfromtxt(i,unpack=True)
+
+	args=np.where((twotheta_deg>=min(tt))&(twotheta_deg<=max(tt)))
+	twotheta_deg,yobs=twotheta_deg[args],yobs[args]
+	yh=f(twotheta_deg)
+
+	plt.close('all')
+	plt.plot(twotheta_deg,yobs)
+	plt.plot(twotheta_deg,yh)
+
+	step,argscut=np.gradient(twotheta_deg)[0],[]
+	for i,valuei in enumerate(yh):
+		if twotheta_deg[i]>min(twotheta_deg)+2 and valuei>min(yh[i-int(2/step):i+int(2/step)])*2:
+			argscut.append(np.arange(i-int(1/2/step),i+int(1/2/step)))
+	Cyh=np.trapz(yobs[argscut].flatten(),x=twotheta_deg[argscut].flatten())/np.trapz(yh[argscut].flatten(),x=twotheta_deg[argscut].flatten())
+	twotheta_deg,yobs,yh=np.delete(twotheta_deg,argscut),np.delete(yobs,argscut),np.delete(yh,argscut)
+
+	yh=signal.savgol_filter(yh,101,1)
+	yobs-=Cyh*yh
+
+	plt.plot(twotheta_deg,yobs)
+	plt.plot(twotheta_deg,Cyh*yh)
+	plt.text(min(twotheta_deg),min(Cyh*yh),Cyh.round(3))
+
+	plt.yscale('log')
+	plt.savefig(filename+'_corr.png')
+
+	np.savetxt(filename+'_bgcorr.xy',np.transpose([twotheta_deg,abs(yobs)]),fmt='%.6f')
