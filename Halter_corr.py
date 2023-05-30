b06085d39d22fc245b9f70eb9d4627ed005b850e
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2023-05-30 12:45:47 +0200

    Add files via upload

--- Halter_corr.py
+++ Halter_corr.py
@@ -1,5 +1,5 @@
 """
-Created 01. February 2023 by Daniel Van Opdenbosch, Technical University of Munich
+Created 30. May 2023 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -19,7 +19,7 @@ else:
 os.system('rm '+os.path.splitext(filepattern)[0]+'*_corr.png')
 os.system('rm '+os.path.splitext(filepattern)[0]+'*_bgcorr.xy')
 
-tt,yh0=np.genfromtxt('Alu_Halter_flach_Miniflex.xy',unpack=True)
+tt,yh0=np.genfromtxt('Stahl_Halter_DCS500.xy',unpack=True)
 f=interpolate.interp1d(tt,yh0)
 
 for i in list(filter(lambda a:'Halter' not in a,glob.glob(os.path.splitext(filepattern)[0]+'*.xy'))):
@@ -28,24 +28,25 @@ for i in list(filter(lambda a:'Halter' not in a,glob.glob(os.path.splitext(filep
 
 	args=np.where((twotheta_deg>=min(tt))&(twotheta_deg<=max(tt)))
 	twotheta_deg,yobs=twotheta_deg[args],yobs[args]
-	yh=f(twotheta_deg)
-
-	plt.close('all')
-	plt.plot(twotheta_deg,yobs)
-	plt.plot(twotheta_deg,yh)
+	yh=signal.savgol_filter(f(twotheta_deg),51,1)
 
 	step,argscut=np.gradient(twotheta_deg)[0],[]
 	for i,valuei in enumerate(yh):
 		if twotheta_deg[i]>min(twotheta_deg)+2 and valuei>min(yh[i-int(2/step):i+int(2/step)])*2:
 			argscut.append(np.arange(i-int(1/2/step),i+int(1/2/step)))
 	Cyh=np.trapz(yobs[argscut].flatten(),x=twotheta_deg[argscut].flatten())/np.trapz(yh[argscut].flatten(),x=twotheta_deg[argscut].flatten())
-	twotheta_deg,yobs,yh=np.delete(twotheta_deg,argscut),np.delete(yobs,argscut),np.delete(yh,argscut)
 
-	yh=signal.savgol_filter(yh,101,1)
-	yobs-=Cyh*yh
-
+	plt.close('all')
 	plt.plot(twotheta_deg,yobs)
 	plt.plot(twotheta_deg,Cyh*yh)
+
+	plt.plot(twotheta_deg,Cyh*yh)
+
+	yobs-=Cyh*yh
+
+	twotheta_deg,yobs=np.delete(twotheta_deg,argscut),np.delete(yobs,argscut)
+
+	plt.plot(twotheta_deg,yobs)
 	plt.text(min(twotheta_deg),min(Cyh*yh),Cyh.round(3))
 
 	plt.yscale('log')
