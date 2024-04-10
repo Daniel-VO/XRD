d71cb09f40ddb64dd5f35ba9e59f8ab68d7a3c01
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-04-10 08:47:21 +0200

    Add files via upload

---
+++ BGMN_setlims.py
@@ -0,0 +1,16 @@
+import os
+import glob
+
+limit=1+float(input('Grenzen Parameter A B C ALPHA BETA GAMMA, in Prozent: '))/100
+
+params=['A','B','C','ALPHA','BETA','GAMMA']
+
+for f in glob.glob('*[!amorphous].str'):
+	for l in open(f):
+		for s in l.split(' '):
+			for item in s.split('='):
+				for p in params:
+					if item==p and 'E=' not in s:
+						startval=float(s.split('=')[-1].split('_')[0])
+						news=s.replace(s.split('=')[-1],str(startval)+'_'+str(round(startval/limit,6))+'^'+str(round(startval*limit,6)))
+						os.system("sed -i 's/"+s+"/"+news+"/g' "+f)
