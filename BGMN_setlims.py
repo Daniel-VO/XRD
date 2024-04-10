b7d383a0ed8c3178187df370bbe9625942d7e27d
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2024-04-10 09:41:16 +0200

    Add files via upload

--- BGMN_setlims.py
+++ BGMN_setlims.py
@@ -5,12 +5,14 @@ limit=1+float(input('Grenzen Parameter A B C ALPHA BETA GAMMA, in Prozent: '))/1
 
 params=['A','B','C','ALPHA','BETA','GAMMA']
 
-for f in glob.glob('*[!amorphous].str'):
-	for l in open(f):
-		for s in l.split(' '):
-			for item in s.split('='):
-				for p in params:
-					if item==p and 'E=' not in s:
-						startval=float(s.split('=')[-1].split('_')[0])
-						news=s.replace(s.split('=')[-1],'%.6f'%startval+'_'+'%.6f'%(startval/limit)+'^'+'%.6f'%(startval*limit))
-						os.system("sed -i 's/"+s+"/"+news+"/g' "+f)
+for f in glob.glob('*.str'):
+	if 'amorphous' not in f:
+		print(f)
+		for l in open(f):
+			for s in l.split(' '):
+				for item in s.split('='):
+					for p in params:
+						if item==p and 'E=' not in s:
+							startval=float(s.split('=')[-1].split('_')[0])
+							news=s.replace(s.split('=')[-1],'%.6f'%startval+'_'+'%.6f'%(startval/limit)+'^'+'%.6f'%(startval*limit))
+							os.system("sed -i 's/"+s+"/"+news+"/g' "+f)
