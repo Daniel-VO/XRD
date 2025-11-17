d30f94634f3189e124084fed19b4927fc17429ad
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2025-11-17 14:59:03 +0100

    Add files via upload

--- BGMN_Vonk.py
+++ BGMN_Vonk.py
@@ -1,5 +1,5 @@
 """
-Created 23. October 2025 by Daniel Van Opdenbosch, Technical University of Munich
+Created 17. November 2025 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -16,7 +16,7 @@ from quantities import UncertainQuantity as uq
 def fsquared(vects,atoms,energy):												#Atomare Streufaktoren
 	return np.real(np.average(np.array([i.f(2*np.pi*vects,en=energy) for i in atoms])**2,axis=0))
 
-def R(vects,yobs,ycoh):														#Vonk R-Funktion
+def R(vects,yobs,ycoh):															#Vonk R-Funktion
 	return scipy.integrate.cumulative_trapezoid(yobs,x=vects)/scipy.integrate.cumulative_trapezoid(ycoh,x=vects)
 
 def T(vects,atoms,energy,yobs,J):												#Vonk T-Funktion
@@ -50,7 +50,7 @@ def Vonk(filename,atoms,yobs,ycoh,twotheta_deg,emission,inelcor,varslitcor):	#Ha
 		params.add('J',1,min=0)
 		def VonkTfitfunc(params):
 			prmT=params.valuesdict()
-			return T(vects,atoms,energy,yobs,prmT['J'])[argsJ]-T(vects,atoms,energy,yobs,prmT['J'])[argsJ][-1]
+			return T(vects,atoms,energy,yobs,prmT['J'])[argsJ]-np.median(T(vects,atoms,energy,yobs,prmT['J'])[argsJ][-10:])
 		resultT=lm.minimize(VonkTfitfunc,params,method='least_squares')
 		prmT=resultT.params.valuesdict()
 		for key in resultT.params:
