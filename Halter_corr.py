c29aad4e50b5d07bd8ef6382f7196ee1f1cca2ab
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2026-02-20 15:17:09 +0100

    Add files via upload

--- Halter_corr.py
+++ Halter_corr.py
@@ -1,5 +1,5 @@
 """
-Created 19. Februar 2026 by Daniel Van Opdenbosch, Technical University of Munich
+Created 20. Februar 2026 by Daniel Van Opdenbosch, Technical University of Munich
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. It is distributed without any warranty or implied warranty of merchantability or fitness for a particular purpose. See the GNU general public license for more details: <http://www.gnu.org/licenses/>
 """
@@ -37,6 +37,7 @@ f=interpolate.interp1d(tt,yh0)
 @ray.remote
 def corr(i):
 	fn=os.path.splitext(i)[0]
+
 	tt_deg,yobs=np.genfromtxt(i,unpack=True)
 	step=np.gradient(tt_deg)[0]
 
@@ -47,39 +48,45 @@ def corr(i):
 	peaks,properties=signal.find_peaks(yh,prominence=100,width=(int(0.5/step),int(1/step)))
 	argscut=[np.arange(p-2*int(properties['widths'][i]),p+2*int(properties['widths'][i])) for i,p in enumerate(peaks)]
 
-	relints=[np.trapz(yobs[a]-min(yobs[a]),x=tt_deg[a])/np.trapz(yh[a]-min(yh[a]),x=tt_deg[a]) for a in argscut]
+	if len(peaks)>1:
+		relints=[np.trapz(yobs[a]-min(yobs[a]),x=tt_deg[a])/np.trapz(yh[a]-min(yh[a]),x=tt_deg[a]) for a in argscut]
+		argscut=np.concatenate(argscut)
 
-	params=lm.Parameters()
-	params.add('mu',0,vary=abscorr)
-	params.add('t',d,vary=False)
-	def fitfunc(params):
-		prm=params.valuesdict()
-		return np.quantile(relints-A(prm['mu'],prm['t'],tt_deg[peaks]),0)
-	results=lm.minimize(fitfunc,params)
-	prm=results.params.valuesdict()
+		params=lm.Parameters()
+		params.add('mu',0,vary=abscorr)
+		params.add('t',d,vary=False)
+		def fitfunc(params):
+			prm=params.valuesdict()
+			return np.quantile(relints-A(prm['mu'],prm['t'],tt_deg[peaks]),0)
+		results=lm.minimize(fitfunc,params)
+		prm=results.params.valuesdict()
 
-	# ~ plt.close('all')
-	# ~ plt.plot(tt_deg[peaks],relints)
-	# ~ plt.plot(tt_deg,A(prm['mu'],prm['t'],tt_deg))
-	# ~ plt.show()
+		# ~ plt.close('all')
+		# ~ plt.plot(tt_deg[peaks],relints)
+		# ~ plt.plot(tt_deg,A(prm['mu'],prm['t'],tt_deg))
+		# ~ plt.show()
 
-	Cyh=A(prm['mu'],prm['t'],180)
-	argscut=np.concatenate(argscut)
-	# ~ Cyh=(max(yobs[argscut])-min(yobs[argscut]))/(max(yh[argscut])-min(yh[argscut]))/2
+		Cyh=A(prm['mu'],prm['t'],180)
+		yobs/=(1-A(prm['mu'],prm['t'],tt_deg))
+	else:
+		Cyh=np.median(yobs[:10]/yh[:10])
 
-	plt.close('all')
-	plt.plot(tt_deg,yobs)
-	plt.plot(tt_deg,yobs/(1-A(prm['mu'],prm['t'],tt_deg)))
-	plt.plot(tt_deg,Cyh*yh)
-	plt.plot(tt_deg[peaks],Cyh*yh[peaks],'+')
-
-	yobs/=(1-A(prm['mu'],prm['t'],tt_deg))
 	yobs-=Cyh*yh
 
 	tt_deg,yobs=np.delete(tt_deg,argscut),np.delete(yobs,argscut)
 
+	# ~ Cyh=(max(yobs[argscut])-min(yobs[argscut]))/(max(yh[argscut])-min(yh[argscut]))/2
+
+	plt.close('all')
+	plt.plot(tt_deg,yobs+Cyh*yh)
+	# ~ plt.plot(tt_deg,yobs/(1-A(prm['mu'],prm['t'],tt_deg)))
+	plt.plot(tt_deg,Cyh*yh)
+	plt.plot(tt_deg[peaks],Cyh*yh[peaks],'+')
 	plt.plot(tt_deg,yobs)
-	plt.figtext(0.15,0.15,'scale=%.3f'%Cyh+'\n mu=%.2f'%prm['mu']+'/m')
+	if len(peaks)>1:
+		plt.figtext(0.15,0.15,'scale=%.3f'%Cyh+'\n mu=%.2f'%prm['mu']+'/m')
+	else:
+		plt.figtext(0.15,0.15,'scale=%.3f'%Cyh)
 
 	plt.yscale('log')
 
